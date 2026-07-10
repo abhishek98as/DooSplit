66e55066fef24e3ce1dc67ed3a50e0979ebb475b
ABHISHEK SINGH <abhishek98as@gmail.com>
2026-07-10 19:30:58 +0530

    Fix local reference error in getActiveRepository due to missing import

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,6 +1,8 @@
+import { dynamodbReadRepository } from "./dynamodb-adapter";
+
 export * from "./config";
 export * from "./types";
-export { dynamodbReadRepository } from "./dynamodb-adapter";
+export { dynamodbReadRepository };
 
 /** Always returns the DynamoDB read repository. */
 export async function getActiveRepository() {
